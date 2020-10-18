581a2b03e34a76d7333fba004de06ddd870ab110
John <john@neerdael.nl>
2020-10-18 09:57:30 +0200

    Update nios.py

--- netmiko/nios/nios.py
+++ netmiko/nios/nios.py
@@ -1,12 +1,12 @@
 """NIOS OS Support"""
-from netmiko.cisco_base_connection import CiscoBaseConnection
+from netmiko.base_connection import BaseConnection
 import time
 
-class InfobloxBase(CiscoBaseConnection):
+class InfobloxBase(BaseConnection):
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
-        self._test_channel_read(pattern=">")
-        self.set_base_prompt(pattern=">")
+        self._test_channel_read(pattern=r"[>#]")
+        self.set_base_prompt()
         self.disable_paging(command="set lines 0")
         # Clear the read buffer
         time.sleep(0.3 * self.global_delay_factor)
@@ -17,6 +17,18 @@ class InfobloxBase(CiscoBaseConnection):
         return super().save_config(
             cmd=cmd, confirm=confirm, confirm_response=confirm_response
         )
+
+    def cleanup(self, command="exit"):
+        """Gracefully exit the SSH session."""
+        try:
+            # The pattern="" forces use of send_command_timing
+            if self.check_config_mode(pattern=""):
+                self.exit_config_mode()
+        except Exception:
+            pass
+        # Always try to send final 'exit' (command)
+        self._session_log_fin = True
+        self.write_channel(command + self.RETURN)
 
 
 class InfobloxSSH(InfobloxBase):
